7e8bd9f341141628afe7a2b5f416880159d68913
HKcode22 <139022998+HKcode22@users.noreply.github.com>
2024-04-22 23:22:56 -0700

    Update Main.java

--- HW/HW7/src/Main.java
+++ HW/HW7/src/Main.java
@@ -1,32 +1,77 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.sound.midi.SysexMessage;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Main {
+    public static int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> min = new PriorityQueue<>();
 
-    public int minMeetingRooms(int[][] intervals) {  // Your code here
+//        for (int[] interval : intervals) {
+        for (int i = 0; i < intervals.length; i++) {
+//            int[] interval = intervals[i];
+            if (!min.isEmpty() && min.peek() <= intervals[i][0]) {
+                min.poll();
 
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        PriorityQueue<Integer> minheap = new PriorityQueue<>();
-
-        minheap.offer(intervals[0][1]);
-
-        for(int i=1; i<intervals.length; i++){
-            if
+            }
+//            System.out.println("Min peak: " + min.peek());
+//            System.out.println("Min: "+ min);
+//            System.out.println("Intervals: " + intervals[i][0]);
+//            System.out.println("Interval: " + interval[0]);
+//            System.out.println("min size: " + min.size());
+//            System.out.println("------------");
+            min.offer(intervals[i][1]);
         }
 
+
+        return min.size();
     }
 
+    public static void main(String[] args) {
+        int[][] intervals1 = {{0,30},{5,10},{15,20}};
+        System.out.println("Minimum Number of Servers Required: " + minMeetingRooms(intervals1));
 
-
-
-        public static void main(String[] args) {
-
-
-
-
-
-
+        int[][] intervals2 = {{0,1},{1,2},{2,3}};
+        System.out.println("Minimum Number of Servers Required: " + minMeetingRooms(intervals2));
     }
 }
+
+
+
+
+
+
+
+
+// // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// // then press Enter. You can now see whitespace characters in your code.
+// import java.util.Arrays;
+// import java.util.PriorityQueue;
+
+// public class Main {
+
+//     public int minMeetingRooms(int[][] intervals) {  // Your code here
+
+//         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+//         PriorityQueue<Integer> minheap = new PriorityQueue<>();
+
+//         minheap.offer(intervals[0][1]);
+
+//         for(int i=1; i<intervals.length; i++){
+//             if
+//         }
+
+//     }
+
+
+
+
+//         public static void main(String[] args) {
+
+
+
+
+
+
+//     }
+// }
